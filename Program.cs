7cfd0c4f876daf9cbc072217e1045d7e2e2d1bef
Antonio Medina <adjmedina@gmail.com>
2020-05-17 19:45:05 -0500

    Mejora en la estuctura del program inicial para hacerlo más comprensible

--- Program.cs
+++ Program.cs
@@ -8,39 +8,50 @@ namespace LeerData
 {
     class Program
     {
-        static void Main(string[] args)
+        /// Lectura una tabla a traves  de una entidad
+        public static void LecturaCurso()
         {
-            /* -- Ejemplos
-            //Ejemplo de lectura de una entidad
+            //Llamada a dbContext - EL que conecta y entiende la db
             using (var db = new AppVentaCursosContext())
             {
-                // Lectura Simple - SIn cache
+                Console.WriteLine("*************************************************");
+                // Lectura de una tabla Simple - Sin cache
                 var cursos = db.Curso.AsNoTracking();
                 foreach (Curso curso in cursos) // arreglo IQueryable
                 {
-                    // String interpolation:
-                    Console.WriteLine($"CURSO:  {curso.Titulo}, Publicado el: {curso.FechaPublicacion}.");
+                    Console.WriteLine($"Curso:  {curso.Titulo}. \nPublicado el: {curso.FechaPublicacion}.");
+                    Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - -");
                 }
+                Console.WriteLine("*************************************************");
             }
+        }
 
-            //Ejemplo de lectura de dos entidades - Uno a Uno
+        //Ejemplo de lectura de dos entidades - Relación Uno a Uno
+        public static void LecturaCursoPrecio()
+        {
+
             using (var db = new AppVentaCursosContext())
             {
+                Console.WriteLine("*************************************************");
+                // El metodo include permite que se haga la consulta a la tabla promocion y lo guarde en la prop del curso
                 var cursos = db.Curso.Include(p => p.PrecioPromocion).AsNoTracking();
                 foreach (Curso curso in cursos) // arreglo IQueryable se pasa al tipo de dato que requiere
                 {
-                    // Composite formatting:
-                    Console.WriteLine("{0} ----------- {1} ", curso.Titulo, curso.PrecioPromocion.PrecioActual);
+                    Console.WriteLine("Curso: {0} - Precio Promoción: ${1} ", curso.Titulo, curso.PrecioPromocion.PrecioActual);
+                    Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - -");
                 }
             }
+        }
 
-            //Ejemplo de lectura de dos entidades - Uno a Muchos
+        //Ejemplo de lectura de dos entidades - Uno a Muchos
+        public static void LecturaCursoComentarios()
+        {
             using (var db = new AppVentaCursosContext())
             {
+                Console.WriteLine("*************************************************");
                 var cursos = db.Curso.Include(c => c.ComentarioLista).AsNoTracking();
                 foreach (Curso curso in cursos) // arreglo IQueryable se pasa al tipo de dato que requiere
                 {
-                    // Composite formatting:
                     Console.WriteLine("{0} ----------- ", curso.Titulo);
                     Console.WriteLine("---------------------------------------------");
                     foreach (Comentario comentario in curso.ComentarioLista)
@@ -50,34 +61,34 @@ namespace LeerData
                     }
                 }
             }
-            --- */
-            // Ejemplo de consulta Muchos a Muchos
-            /* using (var db = new AppVentaCursosContext())
-             {
-                 // En esta linea se hace el cruce de las 3 tablas Curso ¬ CursoInstructor ¬ Instructor
-                 var cursos = db.Curso.Include(c => c.InstructorLink).ThenInclude(ci => ci.Instructor);
-                 foreach (Curso curso in cursos) // arreglo IQueryable se pasa al tipo de dato que requiere
-                 {
-                     Console.WriteLine("**--- {0} ---**", curso.Titulo);
-                     Console.WriteLine("-------------------- Profesores --------------------");
-                     foreach (CursoInstructor instrLink in curso.InstructorLink)
-                     {
-                         Console.WriteLine("Nombre: -> {0} | {1} \n", instrLink.Instructor.Nombre, instrLink.Instructor.Apellidos);
-                     }
+        }
 
-                 }
-             } */
-
-            // Ejemplo de Agragar
+        // Ejemplo de consulta Muchos a Muchos
+        public static void LecturaCursoProfesores()
+        {
             using (var db = new AppVentaCursosContext())
             {
                 // En esta linea se hace el cruce de las 3 tablas Curso ¬ CursoInstructor ¬ Instructor
-                var instrLink = db.Curso.AsNoTracking();
-                foreach (var ins in instrLink) // arreglo IQueryable se pasa al tipo de dato que requiere
+                var cursos = db.Curso.Include(c => c.InstructorLink).ThenInclude(ci => ci.Instructor);
+                foreach (Curso curso in cursos) // arreglo IQueryable se pasa al tipo de dato que requiere
                 {
-                    Console.WriteLine("**--- {0} ---**", ins);
+                    Console.WriteLine("**--- {0} ---**", curso.Titulo);
+                    Console.WriteLine("-------------------- Profesores --------------------");
+                    foreach (CursoInstructor instrLink in curso.InstructorLink)
+                    {
+                        Console.WriteLine("Nombre: -> {0} | {1} \n", instrLink.Instructor.Nombre, instrLink.Instructor.Apellidos);
+                    }
+
                 }
             }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Ejemplo de Lectura
+            LecturaCursoProfesores();
+
 
         }
     }

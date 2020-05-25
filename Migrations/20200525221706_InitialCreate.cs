88df70063526b6e484e907edf936ba6b53f9c57b
Antonio Medina <adjmedina@gmail.com>
2020-05-25 18:06:07 -0500

    Aplicando la primer migracion con dotnet-ef migrations add

---
+++ Migrations/20200525221706_InitialCreate.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace LeerData.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    CursoId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(nullable: true),
+                    Descripcion = table.Column<string>(nullable: true),
+                    FechaPublicacion = table.Column<DateTime>(nullable: false),
+                    FotoPortada = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.CursoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Instructor",
+                columns: table => new
+                {
+                    InstructorId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: true),
+                    Apellidos = table.Column<string>(nullable: true),
+                    Grado = table.Column<string>(nullable: true),
+                    FotoPerfil = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instructor", x => x.InstructorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comentario",
+                columns: table => new
+                {
+                    ComentarioId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Alumno = table.Column<string>(nullable: true),
+                    Puntaje = table.Column<int>(nullable: false),
+                    ComentarioTexto = table.Column<string>(nullable: true),
+                    CursoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentario", x => x.ComentarioId);
+                    table.ForeignKey(
+                        name: "FK_Comentario_Curso_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Curso",
+                        principalColumn: "CursoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Precio",
+                columns: table => new
+                {
+                    PrecioId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    PrecioActual = table.Column<decimal>(nullable: false),
+                    Promocion = table.Column<decimal>(nullable: false),
+                    CursoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Precio", x => x.PrecioId);
+                    table.ForeignKey(
+                        name: "FK_Precio_Curso_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Curso",
+                        principalColumn: "CursoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CursoInstructor",
+                columns: table => new
+                {
+                    InstructorId = table.Column<int>(nullable: false),
+                    CursoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CursoInstructor", x => new { x.InstructorId, x.CursoId });
+                    table.ForeignKey(
+                        name: "FK_CursoInstructor_Curso_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Curso",
+                        principalColumn: "CursoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CursoInstructor_Instructor_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "Instructor",
+                        principalColumn: "InstructorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentario_CursoId",
+                table: "Comentario",
+                column: "CursoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CursoInstructor_CursoId",
+                table: "CursoInstructor",
+                column: "CursoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Precio_CursoId",
+                table: "Precio",
+                column: "CursoId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentario");
+
+            migrationBuilder.DropTable(
+                name: "CursoInstructor");
+
+            migrationBuilder.DropTable(
+                name: "Precio");
+
+            migrationBuilder.DropTable(
+                name: "Instructor");
+
+            migrationBuilder.DropTable(
+                name: "Curso");
+        }
+    }
+}
